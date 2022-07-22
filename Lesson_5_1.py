184fee1545a43cc97c0c2f0441e8037d46b0c489
gbaccount2022 <citro.krd.2020@mail.ru>
2022-07-22 23:56:00 +0300

    fix
    fix

--- Lesson_5_1.py
+++ Lesson_5_1.py
@@ -1,22 +1,53 @@
 
-
-######################################
-# 1.txt - 1 2 3
-# 2.txt - 4 4 4
-# result = 1 + 2 + 3 + 4 + 4 + 4 = 18
-
+# 3*x^3 + 5*x^2 + 10*x + 11
+#         7*x^2        + 55
+# 3*x^3 +12*x^2 + 10*x + 66
 
 def fileToArray(fname):
     with open(fname, 'r') as f:
-        return list(map(lambda x: x, f.read().split()))
+        return list(map(lambda x: x, f.read().replace(' ', '').split('+')))
 
-arr1 = fileToArray('1.txt')
-arr2 = fileToArray('2.txt')
+def maxarr(arr1,arr2):
+    if len(arr1) < len(arr2):
+        return [arr2, arr1]
+    return [arr1, arr2]
 
-arr1.extend(arr2)
+def looksSame(x, rate):
+    if (len(rate) == 0):
+        return x.find('x') == -1
 
-sum = 0
-for index, item in enumerate(arr1):
-    sum += int(item)
+    if len(x) < len(rate):
+        return False
 
-print(f'result: {sum}')
+    pos = x.find(rate)
+    pos1= len(x) - len(rate)
+    return pos == pos1
+
+
+result = []
+[arr1, arr2] = maxarr(fileToArray('2.txt'), fileToArray('1.txt'))
+
+
+for index,item in enumerate(arr1):
+    items1 = item.split('*')
+
+    if (len(items1) > 1):
+        rate = items1[1]
+    else:
+        #
+        rate = ''
+
+    newitem = list(filter(lambda x: looksSame(x, rate), arr2))
+
+    if len(newitem) > 0:
+        newitem = newitem[0]
+        items2 = newitem.split('*')
+        if len(items2) > 1:
+            result.append(str(int(items1[0]) + int(items2[0])) + items1[1])
+        else:
+            result.append(str(int(items1[0]) + int(items2[0])))
+    else:
+        result.append(item)
+
+
+print(f'result: {" + ".join(result)}')
